e08fed5c7846b0250d4ab1cd57771e6abc75a15d
Luke <ljhorner2@gmail.com>
2023-07-08 13:10:14 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html', // Path to your HTML template
+    }),
+  ],
+};
